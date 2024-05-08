8412342aac82ee62d80b1382579605d921e09dc6
kishore <106666554+kishoreology@users.noreply.github.com>
2024-05-08 13:21:08 +0530

    Update QuizGame.py

--- QuizGame.py
+++ QuizGame.py
@@ -122,14 +122,14 @@ class QuizBrain:
         self.check_answer(user_answer, current_question.answer)
     
     def check_answer(self, user_answer, correct_answer):
-    if user_answer.lower() == correct_answer.lower():
-        self.score += 1
-        print("You got it right!")
-    else:
-        print("That's wrong.")
-    print(f"The correct answer was: {correct_answer}.")
-    print(f"Your current score is: {self.score}/{self.question_number}")
-    print("\n")
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
 question_bank = []
 for question in question_data:
     question_text = question["question"]
